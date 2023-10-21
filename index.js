31054e7e1dfd0af3a5db66e420a2d6e0190e4a0c
ED <gg@mail.ru>
2023-10-21 15:51:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let gg = 9999
+// let url = 'https://proverili.ru/api/areas';
+// let res = $.ajax(url, { method: 'POST', dataType: 'json', })
+// console.log(res)
+
+// $.ajax({ 
+//     type: 'POST', 
+//     url: 'https://proverili.ru/api/areas', 
+//     data: { get_param: 'value' }, 
+//     dataType: 'json',
+//     success: function (data) { 
+//         $.each(data, function(index, element) {
+//             mas.push(element)
+//         });
+//     }
+// });
+
+// console.log(mas)
